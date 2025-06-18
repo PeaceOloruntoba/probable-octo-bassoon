900a0ec8eda111bf477ab053a2e5b94d2e2f1de5
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-06-18 17:35:03 +0100

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 import ellipse from "./images/Ellipse 2.png";
-import googleIcon from "./images/iconfinder_Google_1298745 1.png"
+import googleIcon from "./images/iconfinder_Google_1298745 1.png";
+import logo from "./images/logo.png";
 
-export { ellipse, googleIcon };
+export { ellipse, googleIcon, logo };
