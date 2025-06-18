51a4aaeaa5c410e12bca422ef4e012a01154fa38
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-06-18 16:36:24 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ export default function App() {
       <div className="w-1/3 bg-gradient-to-br from-[#0F70DA] to-[#007AFF] h-full flex flex-col items-center justify-center text-white font-semibold text-xl text-center">
         <span>Social media shared today,<br />tomorrow or by location</span>
       </div>
-      <div className="w-2/3 h-full flex flex-col">
+      <div className="w-2/3 h-full flex flex-col p-4">
       <img src="" alt="" />
       <span>Create account</span>
       <span>For business, band or celebrity.</span>
-      <form className=""></form>
+      <form className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" className="outline" />
+        </div>
+      </form>
       </div>
     </div>
   );
